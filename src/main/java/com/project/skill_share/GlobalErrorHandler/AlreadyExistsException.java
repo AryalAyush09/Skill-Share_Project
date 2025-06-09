8ca3e09465091c8b2553124ad37188413893f677
AryalAyush09 <aryalayush34@gmail.com>
2025-06-09 12:05:09 +0545

    Implements Swagger

--- src/main/java/com/project/skill_share/GlobalErrorHandler/AlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.project.skill_share.GlobalErrorHandler;
-
-public class AlreadyExistsException extends RuntimeException {
-    public AlreadyExistsException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/project/skill_share/repository/UserSkillRepository.java
+++
@@ -1,7 +0,0 @@
-package com.project.skill_share.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.project.skill_share.entities.User_Skill;
-
-public interface UserSkillRepository extends JpaRepository<User_Skill, Long> {
-       
-}
